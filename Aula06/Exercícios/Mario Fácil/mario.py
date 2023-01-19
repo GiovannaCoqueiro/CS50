13ecfbfadbcbdd587c9062f78edea91b59a771ad
Giovanna Coqueiro <giovannacoqueirolopes@gmail.com>
2023-01-19 17:01:12 -0300

    Create mario.py

---
+++ Aula06/Exercícios/Mario Fácil/mario.py
@@ -0,0 +1,25 @@
+from cs50 import get_int
+
+
+def main():
+    a = get_altura()
+    for i in range(a):
+        # itera os espaços pra inverter a escada
+        for j in range(a - 1, i, -1):
+            print(" ", end="")
+        # itera os blocos na escada
+        for l in range(0, i + 1, 1):
+            print("#", end="")
+        print()
+
+
+# Garante a altura da escada entre 1 e 8 blocos
+def get_altura():
+    while True:
+        n = get_int("Altura da escada: ")
+        if n >= 1 and n <= 8:
+            break
+    return n
+
+
+main()
